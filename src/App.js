a3fa2ae18cc604374a23e45b70917271ef5486aa
Boris Choque <choqueb.boris@gmail.com>
2022-03-21 23:02:11 -0300

    Entrega 01_ se crea proyecto en react y se crea referencia a la pagina de productos

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ingresa a <code>BC - Ventas</code> para ver nuestra página.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://borischoque.github.io/proyecto_ch_bchoque/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BC - VENTAS
         </a>
       </header>
     </div>
